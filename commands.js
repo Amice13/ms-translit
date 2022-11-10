90d6961f950d1d4324f4ce7d7acb3303b8215c10
Kyrylo Zakharov <kirillzakharov13@gmail.com>
2022-11-10 11:35:47 +0200

    let's try'

---
+++ commands.js
@@ -0,0 +1,2 @@
+!function(){var n={};n.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(n){if("object"==typeof window)return window}}(),Office.onReady((function(){})),("undefined"!=typeof self?self:"undefined"!=typeof window?window:void 0!==n.g?n.g:void 0).action=function(n){n.completed()}}();
+//# sourceMappingURL=commands.js.map
